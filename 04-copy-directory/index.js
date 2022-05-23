7b570653b042bc271451cc3bfd7a40876b3acf43
KochanovNikita <kochanovn207@gmail.com>
2022-05-23 18:34:00 +0300

    feat: add solution 4

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,64 @@
-const fs = require('fs/promises');
+const {stdout} = process;
+const {readdir} = require('fs/promises');
+const {stat, rmdir, mkdir, unlink} = require('fs');
+const {copyFile} = require('node:fs/promises');
+const path = require('path');
+const pathToFolderCopy = path.join(__dirname, 'copy folder');
+const pathToFolderOrigin = path.join(__dirname, 'files');
+
+async function copyFolder(){
+  try{
+    const folder = await readdir(pathToFolderOrigin, {withFileTypes: true});
+    for(const file of folder){
+      copyFile(pathToFolderOrigin + `\\${file.name}`, pathToFolderCopy + `\\${file.name}`);
+    }
+  }catch(err){
+    stdout.write('не копирует\n');
+    stdout.write(err);
+  }
+}
+
+function createFolder(){
+  mkdir(pathToFolderCopy, (err) => {
+    if(err){
+      stdout.write('не создал');
+    }
+    copyFolder();
+  });
+}
+
+function removeFolder(){
+  rmdir(pathToFolderCopy, (err)=>{
+    if(err){
+      stdout.write('не удалил\n');
+    }else{
+      createFolder();
+    }
+  });
+}
+
+async function removeAllFilesinFolder(){
+  const files = await readdir(pathToFolderCopy, {withFileTypes: true});
+  for (const file of files){
+    unlink((pathToFolderCopy + '\\' + file.name), err =>{
+      if(err) stdout.write('не удаляет файл ' + file.name + '\n');
+    });
+  }
+  removeFolder();
+}
+
+async function folderExists(){
+  stat(pathToFolderCopy, (err, stats) => {
+    if(err){
+      createFolder();
+    }else{
+      if(stats.isDirectory()){
+        removeAllFilesinFolder();
+      }else{
+        createFolder();
+      }
+    }
+  });
+}
+
+folderExists();
