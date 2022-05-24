6ea40d504047bca997b8e6cb2ac0be907a91ff3d
KochanovNikita <kochanovn207@gmail.com>
2022-05-24 18:28:21 +0300

    feat: add copy folder in solution 06

--- 06-build-page/index.js
+++ 06-build-page/index.js
@@ -1,40 +1,129 @@
 const path = require('path');
-const {stat, mkdir, rmdir, unlink} = require('fs');
+const {stat, mkdir, unlink} = require('fs');
 const {readdir, appendFile} = require('fs/promises');
-const {createReadStream, readFile} = require('node:fs');
+const {createReadStream} = require('node:fs');
+const {copyFile} = require('node:fs/promises');
 const {stdout} = process;
+const componentsPath = path.join(__dirname, 'components');
+const distPath = path.join(__dirname, 'project-dist');
+const assetsPath = path.join(__dirname, 'assets');
+
 //уважаемый проверяющий, если у вас есть возможность, не проверяйте это задание и не ставьте оценку до вечера среды,
 //контакты для связи: telegrem - @pieliedie3x, discord - halva#1696 \ (kochanovnikita)
 
+async function replaceFooter(template){
+  let footer = '';
+  const stream = createReadStream(componentsPath + '\\footer.html', {encoding: 'utf-8'});
+
+  stream.on('readable', () => {
+    let data = stream.read();
+    if(typeof data === 'string'){
+      footer = data;
+      template = template.replace('{{footer}}', footer);
+      appendFile(distPath+'\\index.html',
+        template,
+        err => {
+          if(err) stdout.write('error');
+        });
+    }
+  });
+}
+
+async function replaceArticles(template){
+  let articles = '';
+  const stream = createReadStream(componentsPath + '\\articles.html', {encoding: 'utf-8'});
+
+  stream.on('readable', () => {
+    let data = stream.read();
+    if(typeof data === 'string'){
+      articles = data;
+      template = template.replace('{{articles}}', articles);
+      replaceFooter(template);
+    }
+  });
+}
+
 async function replaceHeader(template){
-  stdout.write(template[0].length.toString());
-  template.forEach(elem => {
-    appendFile(path.join(__dirname, 'index.html'),
-      elem,
-      err => {
-        if(err) stdout.write('error');
-      });
+  let header = '';
+  const stream = createReadStream(componentsPath + '\\header.html', {encoding: 'utf-8'});
+
+  stream.on('readable', () => {
+    let data = stream.read();
+    if(typeof data === 'string'){
+      header = data;
+      template = template.replace('{{header}}', header);
+      replaceArticles(template);
+    }
   });
 }
 
 async function createIndexHTML(){
-  let template = [];
-  /*const stream = createReadStream(path.join(__dirname, 'template.html'), {start: 0, encoding: 'utf-8'});
+  let template = '';
+  const stream = createReadStream(path.join(__dirname, 'template.html'), {autoClose: true, encoding: 'utf-8'});
   stream.on('readable', () => {
     let data = stream.read();
-    if(data != undefined && data != null){
-      template.push(data);
+    if(typeof data === 'string'){
+      template = data;
+      replaceHeader(template);
     }
-    replaceHeader(template);
-  });*/
-  readFile(path.join(__dirname, 'template.html'), 'utf-8', (err, fileContent)=>{
-    if(err){
-      stdout.write('err');
-    }else{
-      template.push(fileContent);
-    }
-    replaceHeader(template);
   });
 }
 
-createIndexHTML();
+async function copyFolder(pathToDir){
+  try{
+    const folder = await readdir(pathToDir, {withFileTypes: true});
+    for(const file of folder){
+      if(file.isDirectory()){
+        createFolder(pathToDir.replace('assets', 'project-dist') + `\\${file.name}`, pathToDir + `\\${file.name}`);
+        copyFolder(pathToDir + `\\${file.name}`);
+      }else if(file.isFile()){
+        copyFile(pathToDir + `\\${file.name}`,pathToDir.replace('assets', 'project-dist') + `\\${file.name}`);
+      }
+    }
+  }catch(err){
+    stdout.write('не копирует\n');
+    console.log(err);
+  }
+}
+
+function createFolder(distpath, copypath){
+  mkdir(distpath, {recursive: true}, (err) => {
+    if(err){
+      stdout.write('не создал');
+    }
+    copyFolder(copypath);
+  });
+}
+
+async function removeAllFilesinFolder(folderpath){
+  const files = await readdir(folderpath, {withFileTypes: true});
+  for (const file of files){
+    if(file.isDirectory()){
+      removeAllFilesinFolder(folderpath+`\\${file.name}`);
+    }else if(file.isFile()){
+      unlink((folderpath + '\\' + file.name), err =>{
+        if(err) stdout.write('не удаляет файл ' + file.name + '\n');
+      });
+    }
+  }
+}
+
+async function createSolution(){
+  stat(distPath, (err, stats) => {
+    if(err){
+      createFolder(distPath, assetsPath);
+      createIndexHTML();
+    }else{
+      if(stats.isDirectory()){
+        removeAllFilesinFolder(distPath);
+        copyFolder(assetsPath);
+        createIndexHTML();
+      }else{
+        createFolder(distPath, assetsPath);
+        createIndexHTML();
+      }
+    }
+  });
+}
+
+createSolution();
